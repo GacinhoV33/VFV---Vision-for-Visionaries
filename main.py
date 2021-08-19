e203563671bf8f72681b5b137375e1d315bdf438
GacinhoV33 <fgacek@student.agh.edu.pl>
2021-08-19 14:43:45 +0200

    Open file in new window

--- main.py
+++ main.py
@@ -8,20 +8,35 @@ from PIL import Image, ImageTk
 import numpy as np
 from settings import BG_SIZE, X_size, Y_size, AppX_size, AppY_size, AppBG_SIZE
 # from func_app import App
+import cv2
 
+
+Origin_image = r"images/No_img.png"
+Img_name = None
 
 def exit_total():
     Root.destroy()
 
 
-def open_file():
-    global basic_img
-    App.filename = filedialog.askopenfilename(initialdir="/", title="Select a file", filetypes=(("png files", "*.png"), ("jpeg files", "*.jp.*")))
-    basic_img = ImageTk.PhotoImage(Image.open(App.filename))
+def MakeGray(path: str):
+    img = cv2.imread(path)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    plt.imshow(img)
+    plt.gray()
+    plt.show()
 
 
+def show_image(Image):
+    ImageWindow = Toplevel()
+    ImageWindow.title(Img_name)
 
-def App():
+    CreatedImageLabel = Label(ImageWindow, image=Image)
+    CreatedImageLabel.pack()
+
+    ImageWindow.mainloop()
+
+
+def App_func():
     App = Toplevel()
     App.tk.call('wm', 'iconphoto', App._w, logo_img)
     App.geometry(AppBG_SIZE)
@@ -29,23 +44,64 @@ def App():
     app_background_img = ImageTk.PhotoImage(
         (Image.open("images/app_background.jpg")).resize((AppX_size, AppY_size), Image.ANTIALIAS))
 
-    AppCanv = Label(App, image=app_background_img)
-    AppCanv.place(x=0, y=0)
+    AppL = Label(App, image=app_background_img)
+    AppL.place(x=0, y=0)
 
-    No_img = ImageTk.PhotoImage((Image.open("images/No_img.png")).resize((int(AppX_size/2 + AppX_size/4), int(AppY_size/2))), Image.ANTIALIAS)
+    No_img = ImageTk.PhotoImage((Image.open(Origin_image)).resize((int(AppX_size/2 + AppX_size/4), int(AppY_size/2))), Image.ANTIALIAS)
     WorkingArea = Label(App, image=No_img)
     WorkingArea.place(x=AppX_size/8, y=AppY_size/75)
 
-    OpenFileButton = Button(App, text="Load file", padx=AppX_size/100, pady=AppY_size/150, command=open_file)
-    OpenFileButton.place(x=AppX_size - AppX_size/12, y=AppY_size/75)
+    OpenFileButton = Button(App, text="Load file", padx=AppX_size/50, pady=AppY_size/150, command=lambda: open_file(App))
+    OpenFileButton.place(x=AppX_size - AppX_size/9.7, y=AppY_size/75)
+
+    if Origin_image == r"images/No_img.png":
+        SaveFileButton = Button(App, text="Save file", padx=AppX_size/50, pady=AppY_size/150, state='disabled')
+        SaveFileButton.place(x=AppX_size - AppX_size/9.7, y=AppY_size/75 * 10)
+    else:
+        SaveFileButton = Button(App, text="Save file", padx=AppX_size / 50, pady=AppY_size / 150, command=open_file)
+        SaveFileButton.place(x=AppX_size - AppX_size / 9.7, y=AppY_size / 75 * 10)
 
 
+    MakeGrayButton = Button(App, text="Make Gray", padx=AppX_size/100, pady=AppY_size/150, command=lambda: MakeGray(r"{}".format(Origin_image)))
+    MakeGrayButton.place(x=AppX_size/8, y=AppY_size/1.9)
+
+    if Origin_image == r"images/No_img.png":
+        ShowImageButton = Button(App, text="Show Image", padx=AppX_size/100, pady=AppY_size/150, state="disabled")
+        ShowImageButton.place(x=AppX_size - AppX_size/9.7, y=AppY_size/75 * 20)
+    else:
+        ShowImageButton = Button(App, text="Show Image", padx=AppX_size / 100, pady=AppY_size / 150,
+                                 command=lambda: show_image(No_img))
+        ShowImageButton.place(x=AppX_size - AppX_size / 9.7, y=AppY_size / 75 * 20)
+
+    # App.update()
     App.mainloop()
 
 
+def open_file(App):
+    global Origin_image, No_img, WorkingArea, Img_name
+    filename = filedialog.askopenfilename(initialdir="/", title="Select a file",
+                                              filetypes=(("png files", "*.png"), ("jpg files", "*.jpg")))
+
+    if filename == "":
+        Origin_image = "images/No_img.png"
+
+    else:
+        # print(filename)
+
+        Origin_image = filename
+        help_var = 0
+        for num, letter in enumerate(Origin_image, 1):
+            if letter == "/":
+                help_var = num
+        Img_name = Origin_image[help_var:]
+        print(Img_name)
+        # WorkingArea = Label(App, image=No_img)
+        # WorkingArea.place(x=AppX_size / 8, y=AppY_size / 75)
+        App.destroy()
+        App_func()
+
+
 if __name__ == "__main__":
-
-
 
     Root = Tk()
     Root.title("Vision For Visionaries")
@@ -72,7 +128,7 @@ if __name__ == "__main__":
     mainCanv.create_text(X_size/2, Y_size/8, text="Welcome!", font=("Helvetica", 50), fill="white")
 
     #adding button to canvas
-    startButt = Button(Root, text="Start", padx=80, pady=10, command=App)
+    startButt = Button(Root, text="Start", padx=80, pady=10, command=App_func)
     startButt_window = mainCanv.create_window(X_size/2 - 80, Y_size - Y_size/11, anchor="nw", window=startButt)
 
     exitButt = Button(Root, text="Exit", padx=80, pady=10, command=exit_total)
@@ -80,6 +136,10 @@ if __name__ == "__main__":
 
     optionButt = Button(Root, text="Options", padx=80, pady=10)
     optionButt_window = mainCanv.create_window(10, Y_size - Y_size / 11, anchor="nw", window=optionButt)
+
+
+
+    # basic_img = No_img
 
 
 
---
+++ readme.txt
@@ -0,0 +1,6 @@
+
+
+
+Tips:
+
+If you receive error while you are trying load an image, then you should change location/path of this file. The best way to avoid this kind of situation is to always put images that you want use in app, in folder created in the main directory.
